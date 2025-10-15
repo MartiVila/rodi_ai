c973153364de6b595319ab2a86cf241b00751d5d
Martí Lázaro Bagué <lazarobaguemarti@gmail.com>
2025-10-15 18:12:07 +0200

    Update scraper_oneuse.py
    Canvis

--- scraper_oneuse.py
+++ scraper_oneuse.py
@@ -3,20 +3,23 @@ import requests
 from bs4 import BeautifulSoup #Using this library cause the stations are impregentated in the structure of the website
 
 # URL of the R3 line (Vic, Torelló, Centelles, etc.)
-url = "http://www.trenscat.com/renfe/r3_ct.html"
+url = "https://cercanias.info/en/rodalies-catalunya/lines/r1-molins-de-rei-macanet-massanes-by-mataro"
 
-# Make the request
+# Make the request, wich returns the web page
 response = requests.get(url)
+# sort by just the HTML content of the page
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Find stations by looking for <div> elements with a specific class
 stations = []
-station_divs = soup.find_all("div", class_="ctnEstacions")
-for div in station_divs:
-    station_name = div.get_text(strip=True)
+station_lis = soup.find_all("li", class_="group relative min-h-28 pb-3 pl-8")
+for li in station_lis.strings:
+    print(li)
+    station_name = li.get_text(strip=True)
     if station_name:  # Ensure the name is not empty
         stations.append(station_name)
 
+print("Stations found:", stations)
 # Build consecutive connections
 connections = []
 for i in range(len(stations) - 1):
