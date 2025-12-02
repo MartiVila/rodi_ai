7374c229f680f4f8ab3918544947cbb13f3bd554
Marti <lazarobaguemarti@gmail.com>
2025-12-02 16:19:16 +0100

    Encoding post Ignasi

--- Agent/Station_Translator.py
+++ Agent/Station_Translator.py
@@ -9,24 +9,47 @@ class StationTranslator:
     Potser podriem definir enums o dicts d'encoding
     """
 
+
+
+    """
+    Començar amb agafar parametres:
+        Stat_tren=[estació a la que va,  Estacio de la que ve,  delay]
+
+    ->Problema és que el tren no va en hora, i volem que vagi en hora
+
+    """
+
+    line_station=[]
+    delay_translator={"Retard_Nul":0, "Retard_Moderat":1, "Retard_Insalvable":2 }
+
     def __init__(self, n_trains: int, n_nodes: int):
         self.n_trains = n_trains
         self.n_nodes = n_nodes
+        self.line_station=[None for i in range(len(n_nodes))]
         # total number of discrete states
         self.n_states = n_trains * n_nodes
 
 
-    # Podriem multiplicar per 1000 el train id, 4 primers bits siguin train id,
-    # Últims (NS quantes estacions hi ha) 8 bits siguin station id
-    #   Això també permet anar afegint variables sense haver d'alterar agent
-    #   Simplement anem multiplicant pq tot quadri i ja està
-    def encode(self, train_id: int, node_id: int) -> int:
+    def encode(self, train_id: int, node_id: int, delay: int) -> int:
         """
-        Map (train_id, node_id) -> state index in [0, n_states-1].
-        Adapt this as your environment becomes more complex
-        (e.g., include delay, direction, etc.).
         """
-        return train_id * self.n_nodes + node_id
+
+        encoded_delay=0
+        encoded_pre_station=0
+        encoded_post_station=0
+
+        if delay < 15:
+            encoded_delay=self.delay_translator["Retard_Nul"]
+
+        elif delay > 30:
+            encoded_delay=self.delay_translator["Retard_Insalvable"]
+
+        else:
+            encoded_delay=self.delay_translator["Retard_Moderat"]
+        
+
+
+        return encoded_post_station*100+encoded_pre_station*10+encoded_delay
 
     def decode(self, state: int) -> tuple[int, int]:
         """
